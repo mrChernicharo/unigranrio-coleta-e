22a980a1d8a3e9f248becf344485f5254b6b0788
Felipe Chernicharo <string7dev@gmail.com>
2022-04-19 23:17:05 -0400

    starting with next-auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ const s = {
 		'group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500',
 };
 
-const Home: NextPage = () => {
+const SignIn: NextPage = () => {
 	return (
 		<div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
 			<div className="max-w-md w-full space-y-8">
@@ -83,4 +83,4 @@ const Home: NextPage = () => {
 	);
 };
 
-export default Home;
+export default SignIn;
