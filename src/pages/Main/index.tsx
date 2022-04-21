7c87aa3f0e00ab54ca7380a54c42e874027a3f52
Felipe Chernicharo <string7dev@gmail.com>
2022-04-21 00:19:03 -0400

    retrieve entries from database

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,10 +1,33 @@
+import { CollectionPoint } from '@prisma/client';
+import { GetServerSideProps } from 'next';
 import Profile from '../../components/Profile';
+import { prisma } from '../../lib/prisma';
 
-export default function App() {
+interface Props {
+	collectionPoints: CollectionPoint[];
+}
+
+export default function App({ collectionPoints }: Props) {
+	console.log(collectionPoints);
 	return (
 		<div className="">
 			<Profile />
 			<h1>Hello world</h1>
+
+			<pre>{JSON.stringify(collectionPoints, null, 2)}</pre>
 		</div>
 	);
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+	const response = await prisma.collectionPoint.findMany();
+
+	const collectionPoints = response.map(item => ({
+		...item,
+		createdAt: item.createdAt.toISOString(),
+	}));
+
+	return {
+		props: { collectionPoints },
+	};
+};
